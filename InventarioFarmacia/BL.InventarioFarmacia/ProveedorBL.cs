93c652f32ef17c3f21ca51b7fabd71a47a6daa2f
alreyes23 <alreyes@cemcol.com>
2020-02-29 15:57:08 -0600

    Csmbios en creacion de clases BL

--- InventarioFarmacia/BL.InventarioFarmacia/ProveedorBL.cs
+++ InventarioFarmacia/BL.InventarioFarmacia/ProveedorBL.cs
@@ -15,6 +15,7 @@ namespace BL.InventarioFarmacia
         public ProveedorBL()
         {
             ListaProveedor = new BindingList<Proveedor>();
+
             var Proveedor1 = new Proveedor();
             Proveedor1.Id = 1;
             Proveedor1.ProveedorNIC = "0190-0005-014523";
@@ -22,9 +23,25 @@ namespace BL.InventarioFarmacia
             Proveedor1.ClienteDireccion = "Col. Montefresco";
             Proveedor1.ClienteTelefono = "+50499044889";
 
-
             ListaProveedor.Add(Proveedor1);
 
+            var Proveedor2 = new Proveedor();
+            Proveedor2.Id = 2;
+            Proveedor2.ProveedorNIC = "0189-0045-0122153";
+            Proveedor2.ClienteCorreo = "alreyes23@hotmail.com";
+            Proveedor2.ClienteDireccion = "Col. Montefresco #2";
+            Proveedor2.ClienteTelefono = "+50499055485";
+
+            ListaProveedor.Add(Proveedor2);
+
+            var Proveedor3 = new Proveedor();
+            Proveedor3.Id = 3;
+            Proveedor3.ProveedorNIC = "0105-04856-152268";
+            Proveedor3.ClienteCorreo = "alreyes@unah.hn";
+            Proveedor3.ClienteDireccion = "Col. Montefresco #3";
+            Proveedor3.ClienteTelefono = "+504990445479";
+
+            ListaProveedor.Add(Proveedor3);
         }
         public BindingList<Proveedor> ObtenerProveedor()
         {
